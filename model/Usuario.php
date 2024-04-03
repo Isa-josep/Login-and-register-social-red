7d4bb8b9fcfff10bf30a9c9ca49d0a9fd080ea07
Isa-josep <isaurini1902@gmail.com>
2024-04-03 13:19:23 -0600

    implementacion de registro

---
+++ model/Usuario.php
@@ -0,0 +1,18 @@
+<?php
+    class Uruario extends Conectar{
+        public function registrar_usuario($usu_nombre, $usu_correo, $usu_pass){
+            $conectar= parent::conexion();
+            parent::set_names();
+            $sql="INSERT INTO tm_usuario 
+                (usu_nombre,usu_correo,usu_pass) 
+                VALUES 
+                ('?' , '?','?')";
+            $sql=$conectar->prepare($sql);
+            //TODO: cmabiar parametros en caso de se necesario
+            $sql->bindValue(1, $usu_nombre);
+            $sql->bindValue(2, $usu_correo);
+            $sql->bindValue(3, $usu_pass);
+            $sql->execute();
+        }
+    }
+?>
