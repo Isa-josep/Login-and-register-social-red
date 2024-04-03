ccab4d71257295697df3a86242c07323424e6747
Isa-josep <isaurini1902@gmail.com>
2024-04-03 13:19:44 -0600

    conexion a la base de datos

--- config/conexion.php
+++ config/conexion.php
@@ -1,8 +1,8 @@
 <?php
-    
+    session_start();
     class Conectar{
         protected $dbh;
-        protected function Conexion(){
+        protected function conexion(){
             try{
                 $conectar= $this->dbh = new PDO("mysql:local=localhost;dbname=tec_export","root","",);
                 return $conectar;
@@ -14,9 +14,11 @@
         }   
         public function set_names(){
             return $this->dbh->query("SET NAMES 'utf8'");
+
         } 
         public static function ruta(){
-
+            //TODO: Cambiar la ruta de acuerdo al servidor
+            return "http://localhost/login/";
         }
     }
 ?>
