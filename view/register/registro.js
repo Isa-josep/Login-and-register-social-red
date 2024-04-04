7ecc9b073b6b21e5b915918e3caa157021c5dad4
Isa-josep <isaurini1902@gmail.com>
2024-04-04 14:47:43 -0600

    Add form validation functions and console log message

--- view/register/registro.js
+++ view/register/registro.js
@@ -3,6 +3,7 @@ function init(){
         e.preventDefault();
         if(isFormValid()){
             register(e);
+            console.log("form valid");
         }
         else{
             console.log("form invalid");
@@ -14,13 +15,35 @@ function init(){
 
 function isFormValid(){
 
-    return validateEmail();
+    return validateEmail() && validateText("usu_nombre") && validatePassword() && validatePasswordMacht();
 }
 
 function validateEmail(){
     var email= $("#usu_correo").val();
     var isValid = validator.isEmail(email);
     showErrorMessage("#usu_correo",isValid,"El correo no es válido");
+    return isValid;
+}
+
+function validateText(fieldID){
+    var value = $("#"+fieldID).val();
+    var isValid = validator.isLength(value,{min:4});
+    showErrorMessage("#"+fieldID ,isValid,"Ingrese Un Nombre De Minimo 4 Caracteres");
+    return isValid;
+}
+
+function validatePassword(){
+    var password = $("#usu_pass").val();
+    var isValid = validator.isLength(password,{min:8});
+    showErrorMessage("#usu_pass",isValid,"Ingrese Minimo 8 Caracteres");
+    return isValid;
+}
+
+function validatePasswordMacht(){
+    var password = $("#usu_pass").val();
+    var password2 = $("#usu_pass_confir").val();
+    var isValid = validator.equals(password,password2);
+    showErrorMessage("#usu_pass_confir",isValid,"Las contraseñas no coinciden");
     return isValid;
 }
 
@@ -32,7 +55,9 @@ function showErrorMessage(fieldSelector,isValid,message){
 
 function displayValidationMessages(){
     validateEmail();
-
+    validateText("usu_nombre");
+    validatePassword();
+    validatePasswordMacht();
 }
 
 function register(e){
