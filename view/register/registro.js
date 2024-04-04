03316e80b37627ac087208a8005ad94a9c5462c2
Isa-josep <isaurini1902@gmail.com>
2024-04-03 20:50:46 -0600

    validate email

--- view/register/registro.js
+++ view/register/registro.js
@@ -1,7 +1,38 @@
 function init(){
     $("#mnt_form").on("submit", function(e){
-        register(e);
+        e.preventDefault();
+        if(isFormValid()){
+            register(e);
+        }
+        else{
+            console.log("form invalid");
+            displayValidationMessages();
+            alert("Formulario inválido");
+        }
     });
+}
+
+function isFormValid(){
+
+    return validateEmail();
+}
+
+function validateEmail(){
+    var email= $("#usu_correo").val();
+    var isValid = validator.isEmail(email);
+    showErrorMessage("#usu_correo",isValid,"El correo no es válido");
+    return isValid;
+}
+
+function showErrorMessage(fieldSelector,isValid,message){
+    var errorField =$(fieldSelector).next(".validation-error");
+    errorField.text(isValid ? "" : message);
+
+}
+
+function displayValidationMessages(){
+    validateEmail();
+
 }
 
 function register(e){
