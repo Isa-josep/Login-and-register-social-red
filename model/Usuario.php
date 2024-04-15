440a8a92aa5b2528ad1ac0bb37887a989a4021a9
Isa-josep <isaurini1902@gmail.com>
2024-04-14 23:21:14 -0600

    Add activate_user function and update confirmar.js and index.php

--- model/Usuario.php
+++ model/Usuario.php
@@ -1,5 +1,8 @@
 <?php
     class Usuario extends Conectar{
+        private $key="Tec-Export-Itsu";
+        private $cipher = "aes-256-cbc";
+
         public function registrar_usuario($usu_nombre, $usu_correo, $usu_pass){
             $conectar= parent::conexion();
             parent::set_names();
@@ -38,6 +41,18 @@
             $sql->bindValue(1, $usu_id);
             $sql->execute();
             return $sql->fetchAll();
+        }
+
+        public function activate_user($usu_id){
+            $iv_dec=substr(base64_decode($usu_id),0,openssl_cipher_iv_length($this->cipher));
+            $cifrado=substr(base64_decode($usu_id),openssl_cipher_iv_length($this->cipher));
+            $txt_descifrado=openssl_decrypt($cifrado,$this->cipher,$this->key,OPENSSL_RAW_DATA,$iv_dec);
+            $conectar= parent::conexion();
+            parent::set_names();
+            $sql="UPDATE tm_usuario SET estado=1, fecha_activate=NOW() WHERE usu_id=?";
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1, $txt_descifrado);
+            $sql->execute();
         }
     }
 ?>
