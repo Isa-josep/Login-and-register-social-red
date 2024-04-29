599f1698041a92a02ca33fe47d57b6c20e50fec2
Isa-josep <isaurini1902@gmail.com>
2024-04-28 23:05:09 -0600

    proteccion de rutas

--- view/home/index.php
+++ view/home/index.php
@@ -1,1 +1,25 @@
-<h1>JAlo</h1>
+<?php
+    require_once("../../config/conexion.php");
+    require_once("../../model/Usuario.php");
+    $usuario= new Usuario();
+    if(isset($_SESSION["usu_id"]) && count($_SESSION)>0){
+        
+    ?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1></h1>Bienvenido <?php echo $_SESSION["usu_nombre"];?></h1>
+    </body>
+    </html>
+    <?php
+    }
+    else{
+        header("Location:".Conectar::ruta()."index.php?m=5");
+        exit();
+    }
+?>
