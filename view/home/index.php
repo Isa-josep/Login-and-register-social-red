4c558036fbe530c12465f6de312bb3aaa6372064
Isa-josep <isaurini1902@gmail.com>
2024-05-11 00:16:42 -0600

    roles index

--- view/home/index.php
+++ view/home/index.php
@@ -2,121 +2,13 @@
     require_once("../../config/conexion.php");
     require_once("../../model/Usuario.php");
     $usuario= new Usuario();
-    if(isset($_SESSION["usu_id"]) && count($_SESSION)>0 ){
-        
-    ?>
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="static/css/styles.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Livvic">
-    <link rel="shortcut icon" href="static/img/_260c3a3f-b5a7-4736-b622-501bce489d78.jpeg" type="image/x-icon">
-    <title>Tec Export</title>
-</head>
-    <body>
-    
-        <div class="navbar">
-            <span class="title">Tec Export.</span> 
-            <ul>
-                <li><a class="itemList" href="index.php">Inicio</a></li>
-                <li><a class="itemList" href="https://tecuruapan.edu.mx/" target="_blank">Nosotros</a></li>
-                <li><a class="itemList" href="#">Productos</a></li>
-                <!-- <li><a class="itemList" href="#">Proyectos</a></li> -->
-                <li><a class="itemList" href="#">Servicios</a></li>
-                <li><a class="itemList" href="#">Noticias</a></li>
-                <li><a class="itemList" href="#">Contacto</a></li>
-                <li><a class="itemList" href="#">Gestion de Usuarios</a></li>
-            </ul>
-        </div>
-        <div class="container">
-            <span class="sub-title">Todo Para Exportar.</span>
-            <div class="sizedBox"></div>
-            <span class="p-sub">Todo lo que necisitas para la exportacion de frutos.</span>
-            <div class="sizedBox"></div>    
-            <button type="button" class="button" id="button">Conoce mas <i class="icon">🥑</i></button>
-            
-        </div>
-        <div class="section">
-            <div class="sub-section">
-                <div class="sizedBox"></div>
-                <div class="sizedBox"></div>
-                <div class="sizedBox"></div>
-                <div class="sizedBox"></div>
-                <div class="card-img">
-                    <div class="sub-section-right">
-                        <div>
-                            <span class="title-right">Que encontraras aqui.</span>
-                            <div class="sizedBox"></div>
-                            <div class="sizedBox"></div>
-                            <span class="sub-right">Todo lo relacionado con la exportacion de Frutos</span>
-                            <div class="sizedBox"></div>
-                            <img src="static/img/icon/material-symbols_check-circle.png" alt="check-box" class="img-right">
-                            <span class="sub-right">Requisitos para la exportacion</span>
-                            <div class="sizedBox"></div>
-                            <img src="static/img/icon/material-symbols_check-circle.png" alt="check-box" class="img-right">
-                            <span class="sub-right">Productos de calidad</span>
-                            <div class="sizedBox"></div>
-                            <img src="static/img/icon/material-symbols_check-circle.png" alt="check-box" class="img-right">
-                            <span class="sub-right">Asesoria en exportacion</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="sizedBox"></div>
-            <div class="sizedBox"></div>
-            <div class="sizedBox"></div>
-            <div class="sizedBox"></div>
-            <div class="sizedBox"></div>
-            <div class="divc">
-                <span class="title-down">Productos mas famosos de Michoacan</span>
-            </div>
-            <div class="down-section">
-                
-                <div class="card-fruit">
-                    <div class="card-fruit-img">
-                        <img src="static/img/icon/icon-Strawberry.png" alt="fresa" class="img-fruit">
-                    </div>
-                    <span class="name-card-fluit">Fresa</span>
-                </div>
-
-                <div class="card-fruit">
-                    <div class="card-fruit-img">
-                        <img src="static/img/icon/peach.png" alt="aguacate" class="img-fruit">
-                    </div>
-                    <span class="name-card-fluit">Durazno</span>
-                </div>
-
-                <div class="card-fruit">
-                    <div class="card-fruit-img">
-                        <img src="static/img/icon/avocado.png" alt="aguacate" class="img-fruit">
-                    </div>
-                    <span class="name-card-fluit">Aguacate</span>
-                </div>
-
-                <div class="card-fruit">
-                    <div class="card-fruit-img">
-                        <img src="static/img/icon/berry.png" alt="aguacate" class="img-fruit">
-                    </div>
-                    <span class="name-card-fluit">Zarzamora</span>
-                </div>
-                
-                <div class="card-fruit">
-                    <div class="card-fruit-img">
-                        <img src="static/img/icon/plum.png" alt="aguacate" class="img-fruit">
-                    </div>
-                    <span class="name-card-fluit">Ciruela</span>
-                </div>
-                
-            </div>
-        </div>
-    </body>
-<script src="static/js/index.js"></script>
-</html>
-<?php
+    if(isset($_SESSION["usu_id"]) && count($_SESSION)>0 && $_SESSION["role_id"]==3){
+        include_once '../html/shared/view_admin.php';
     }
-    if( $_SESSION["estado"]==2){
+    else if(isset($_SESSION["usu_id"]) && count($_SESSION)>0){
+        include_once '../html/shared/view_user.php';
+    }
+    else if( $_SESSION["estado"]==2){
         header("Location:".Conectar::ruta()."index.php?m=6");
         exit(); 
     }
